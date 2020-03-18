7599a92665e9c87dc51375d2467ae72000e93fcc
Oleg Vasilev <oleg.vasilev@intel.com>
2020-03-18 21:44:33 +0300

    Prepare for distributed agent

--- run.py
+++ run.py
@@ -34,13 +34,13 @@ class MonoAgent():
         self.alpha = 0.02
 
     def do_step(self):
-        self.x_ -= self.alpha*problem.grad(self.x_)
+        self.x_ -= self.alpha*self.problem_.grad(self.x_)
 
     def run(self):
         prev_value = 1e9
         for i in range(1000):
             self.do_step()
-            value = problem.apply(self.x_)
+            value = self.problem_.apply(self.x_)
             # print("Value", value)
 
             if abs(value - prev_value) < 1e-9:
@@ -51,6 +51,13 @@ class CommunicationGraph():
     def __init__(self, matrix):
         self.matrix = matrix 
         self.queues = [[] for i in range(len(matrix))]
+
+    def get_incdence_list(v):
+        result = []
+        for i in range(len(self.matrix)):
+            if self.matrix[v][i]:
+                result.append(i)
+
 
     def send(self, fr, to, x):
         if self.matrix[fr][to] != 1:
@@ -63,24 +70,31 @@ class CommunicationGraph():
             return None
         return self.queues[to].pop()
 
+
+def solve_mono_agent(A, b):
+    problem = LinearOptProblem(A, b)
+    agent = MonoAgent(problem)
+    agent.run()
+    result_my = mean_squared_error(A.dot(agent.x_), b)
+    print(agent.x_)
+    print(result_my)
+
+def solve_sklearn(A, b):
+    model = LinearRegression(fit_intercept=False)
+    model.fit(A, b)
+    result_sklearn = mean_squared_error(model.predict(A), b)
+    print(model.coef_)
+    print(result_sklearn)
+
 if __name__=="__main__":
     x, y = 10,15
 
     A = np.random.rand(x,y)
     b = np.random.rand(x)
+    solve_mono_agent(A, b)
+    solve_sklearn(A, b)
 
-    model = LinearRegression(fit_intercept=False)
-    model.fit(A, b)
-    result_sklearn = mean_squared_error(model.predict(A), b)
-    
-    problem = LinearOptProblem(A, b)
-    agent = MonoAgent(problem)
-    agent.run()
-    result_my = mean_squared_error(A.dot(agent.x_), b)
-    print(model.coef_)
-    print(result_sklearn)
-    print(agent.x_)
-    print(result_my)
+
 
 
 
